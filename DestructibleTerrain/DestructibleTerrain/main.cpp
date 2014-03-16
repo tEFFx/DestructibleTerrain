8b09c0d7b2f04e82eb62c2cfa749b8ae0af6c0a5
tEFFx <erikkoii@gmail.com>
2014-03-16 17:40:32 +0100

    background bananas

--- DestructibleTerrain/DestructibleTerrain/main.cpp
+++ DestructibleTerrain/DestructibleTerrain/main.cpp
@@ -4,6 +4,8 @@
 int main()
 {
     sf::RenderWindow window(sf::VideoMode(1280, 720), "Destructible Terrain");
+	sf::Texture bgTexture;
+	bgTexture.loadFromFile("background.png");
 
     while (window.isOpen())
     {
@@ -20,6 +22,7 @@ int main()
 		}
 
         window.clear();
+		window.draw(sf::Sprite(bgTexture));
 		Terrain::getInstance().draw(window);
         window.display();
     }
