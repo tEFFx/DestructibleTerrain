aa463b32d3d3a433d73173afbb995dfce44d1379
tEFFx <erikkoii@gmail.com>
2014-03-17 22:16:29 +0100

    i dont even

--- DestructibleTerrain/DestructibleTerrain/Bazooka.cpp
+++ DestructibleTerrain/DestructibleTerrain/Bazooka.cpp
@@ -5,7 +5,7 @@ Bazooka::Bazooka(sf::Vector2f pos)
 {
 	mHitbox.setPosition(pos);
 	mHitbox.setSize(sf::Vector2f(100, 20));
-	mHitbox.setOrigin(20, 15);
+	mHitbox.setOrigin(20, 10);
 	mHitbox.setFillColor(sf::Color::Black);
 }
 
